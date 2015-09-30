6fe1a15c7eb972bed12c3aa808853ea61d298dc5
Gennadiy Filatov <gfilatov@cpan.org>
2015-09-30 20:49:26 +0300

    some attribtues are missing

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
+name             "psgi"
 maintainer       "Alexey Melezhik"
 maintainer_email "melezhik@gmail.com"
 license          "All rights reserved"
 description      "Configures and runs psgi application"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.2"
+
+recipe "psgi", "Configures and runs psgi application"
 
 %w{ ubuntu debian centos }.each do |os|
   supports os
